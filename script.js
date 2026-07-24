2731750804511c63d1933d3d722f770e3151b438
krish73a-afk <krish73a@gmail.com>
2026-07-24 17:51:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function showPopup(event) {
+    event.preventDefault();
+
+    // Show popup
+    document.getElementById("popup").style.display = "flex";
+
+    // Fade out page
+    document.body.classList.add("fade-out");
+
+    // OPTIONAL: redirect after 2 sec
+    setTimeout(() => {
+        window.location.href = "index.html"; // change page if you want
+    }, 20000);
+}
